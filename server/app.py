aa4567b1cb776b8de109ec30cbf26f77209fb74b
maurice diong <mdiong00@gmail.com>
2024-03-30 02:51:15 -0400

    complete Chatterbox

--- server/app.py
+++ server/app.py
@@ -14,13 +14,46 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages', methods = ['GET', 'POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages = []
+        for message in Message.query.order_by('created_at').all():
+            messages.append(message.to_dict())
+        return make_response(messages, 200) 
+    elif request.method == 'POST':
+        body = request.get_json()['body']
+        username = request.get_json()['username']
+        new_message = Message(body=body, username=username)
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        new_message_dict = new_message.to_dict()
+        response = make_response(new_message_dict, 201)
+        return response
+
+
+@app.route('/messages/<int:id>', methods = ['GET', 'PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id = id).first()
+    if request.method == 'GET':  
+        response = make_response(message.to_dict(), 200)
+        return response
+    elif request.method == 'PATCH':
+        for attr in request.get_json():
+            setattr(message, attr, request.get_json()[f'{attr}'])
+        db.session.add(message)
+        db.session.commit()
+        response = make_response(message.to_dict(),200)
+        return response
+    elif request.method == 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        body = {'delete_successful': True,
+                'message': 'message delete'}
+        response = make_response(body, 200)
+        return response
+    
 
 if __name__ == '__main__':
     app.run(port=5555)
